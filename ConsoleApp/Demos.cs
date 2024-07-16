1709b98982247b0fcb8ba24605a672582f3e3e36
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-16 14:22:20 +0200

    foreach

--- ConsoleApp/Demos.cs
+++ ConsoleApp/Demos.cs
@@ -596,5 +596,61 @@
 
             return userValue; //return służy do przerwania funkcji i zwrócenia wskazanej wartości jako wynik działania funkcji
         }
+
+        public void For()
+        {
+            //I - inicjalizacja pętli - wykonuje się tylko raz na początku
+            //II - warunek kontynukacji pętli - wykonuje się przed każdym wykonaniem ciała
+            //III - ciało pętli
+            //IV - akcja po wykonaniu ciała - najczęściej inkrementacja licznika
+
+            //for(I; II; IV )
+            //   { III; }
+
+            for (int i = 0; i < 5; i++ /*i = i + 1*/)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //pętla nieskończona - odpowiednik while(true)
+            /*for (; ; )
+            {
+                Console.WriteLine("*");
+            }*/
+
+            Console.WriteLine("---");
+
+
+            int value = 5;
+            for (; value > 0; value--)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine("---");
+
+            value = 0;
+            for (; value < 5;)
+            {
+                Console.WriteLine(++value);
+            }
+
+            Console.WriteLine("---");
+
+            value = 0;
+            for (; value < 5;)
+            {
+                Console.WriteLine(value++);
+            }
+
+            string input = Console.ReadLine();
+            string[] splittedInput = input.Split();
+
+            for (int i = 0; i < splittedInput.Length; i++)
+            {
+                Console.WriteLine(splittedInput[i]);
+            }
+        }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,58 +8,29 @@ Demos demos = new Demos();
 //demos.Arrays();
 //demos.IfElse();
 //demos.WhileDoWhile();
+//demos.For();
 
 //Console.ReadLine();
 
-//I - inicjalizacja pętli - wykonuje się tylko raz na początku
-//II - warunek kontynukacji pętli - wykonuje się przed każdym wykonaniem ciała
-//III - ciało pętli
-//IV - akcja po wykonaniu ciała - najczęściej inkrementacja licznika
-
-//for(I; II; IV )
-//   { III; }
-
-for(int i = 0; i < 5; i++ /*i = i + 1*/)
-{
-    Console.WriteLine(i);
-}
 
 
-//pętla nieskończona - odpowiednik while(true)
-/*for (; ; )
-{
-    Console.WriteLine("*");
-}*/
-
-Console.WriteLine("---");
-
-
-int value = 5;
-for(; value > 0; value--)
-{
-    Console.WriteLine(  value );
-}
-
-Console.WriteLine("---");
-
-value = 0;
-for (; value < 5;)
-{
-    Console.WriteLine(++value);
-}
-
-Console.WriteLine("---");
-
-value = 0;
-for (; value < 5;)
-{
-    Console.WriteLine(value++);
-}
-
+Console.WriteLine("Napisz coś:");
 string input = Console.ReadLine();
-string[] splittedInput = input.Split();
 
-for (int i = 0; i < splittedInput.Length; i++)
+string[] words = input.Split();
+
+//foreach zastępuje poniższy sposób iteracji po tablicy
+for (int i = 0; i < words.Length; i++)
 {
-    Console.WriteLine(splittedInput[i]);
+    string word = words[i];
+
+    Console.WriteLine(word);
 }
+
+
+//foreach - pozwala przejsc po wszystkich elementach tablicy
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
