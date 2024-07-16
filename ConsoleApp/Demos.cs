dc0ebb684b2ebb5d13c2bc73747f860b7e917e62
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-16 10:34:45 +0200

    Listy

--- ConsoleApp/Demos.cs
+++ ConsoleApp/Demos.cs
@@ -263,5 +263,65 @@
             floatValue = (float)doubleValue;
             Console.WriteLine(floatValue);
         }
+
+        public void Arrays()
+        {
+            int a = 5;
+            int b = 10;
+            int c = 15;
+
+
+            //deklaracja tablicy przechowującej dane typu int
+            //[] - tablica
+            int[] array;
+
+
+            //inicjalizujemy zmienną nową tablicą typu int o rozmiarze 3
+            //tablica wypełniana jest wartościami domyślnymi zadeklarowanego typu (dla int jest to 0)
+            array = new int[3];
+
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+
+
+            //tablice są indeksowane od 0 (minimalny indeks)
+            //odwołujemy się do pierwszgo elemntu tablicy, czyli pod indeks 0
+            array[0] = 5;
+            array[1] = b;
+            //maksymalny indeks tablicy to rozmiar minus 1 (3 - 1 = 2)
+            //array[2] = c;
+            array[array.Length - 1] = c;
+
+
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[0]);
+
+            Console.WriteLine($"Tablica ma rozmiar {array.Length}");
+
+            string[] inputs = new string[2];
+            Console.WriteLine("Podaj pierwszy bok prostokąta:");
+            inputs[0] = Console.ReadLine();
+            Console.WriteLine("Podaj drugi bok prostokąta:");
+            inputs[1] = Console.ReadLine();
+
+            float[] sides = new float[inputs.Length];
+            sides[0] = float.Parse(inputs[0]);
+            sides[1] = float.Parse(inputs[1]);
+
+            float area = sides[0] * sides[1];
+            Console.WriteLine($"Pole prostokąta to {area}");
+
+            Console.WriteLine("Napisz coś:");
+            string input = Console.ReadLine();
+            //tablica zwracana jako rezultat wywołania metody
+            //metoda split dzieli string na części wg wskazanego separatora
+
+            //string[] words = input.Split(" ");
+            string[] words = input.Split(); //bez parametru, działa jak powyżej
+
+            Console.WriteLine($"Napisałeś {words.Length} słów. Pierwsze słowo: {words[0]}, ostatnie: {words[words.Length - 1]}");
+        }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,68 +1,53 @@
 ﻿using ConsoleApp;
-using System.Globalization;
-using System.Reflection.Metadata;
+using Microsoft.VisualBasic;
+
 
 Demos demos = new Demos();
 //demos.Strings();
 //demos.NumbersFormatting();
 //demos.Numbers();
+//demos.Arrays();
 
 //Console.ReadLine();
 
 
-int a = 5;
-int b = 10;
-int c = 15;
+//tworzymy nową listę. Lista po inicjalizacji jest pusta.
+List<string> listOfStrings = new List<string>();
+//List<string> listOfStrings = new();
+//List<string> listOfStrings = [];
+
+Console.WriteLine(listOfStrings.Count);
+
+//dodajemy nowy element do listy, rozmiar tablicy się zwiększa
+listOfStrings.Add("!");
+listOfStrings.Add("ala");
+listOfStrings.Add("kota");
+
+Console.WriteLine(listOfStrings.Count);
 
 
-//deklaracja tablicy przechowującej dane typu int
-//[] - tablica
-int[] array;
+//stawiamy element na konkretny indeks listy - pozostałe ementy przesuwają się
+listOfStrings.Insert(2, "ma");
 
 
-//inicjalizujemy zmienną nową tablicą typu int o rozmiarze 3
-//tablica wypełniana jest wartościami domyślnymi zadeklarowanego typu (dla int jest to 0)
-array = new int[3];
+listOfStrings.Add("!");
+listOfStrings.Add("!");
 
-Console.WriteLine(array[0]);
-Console.WriteLine(array[1]);
-Console.WriteLine(array[2]);
+//usuwamy element pod indeksem 2 - rozmiar listy się zmniejsza
+listOfStrings.RemoveAt(2);
 
+//usuwamy element wg wartości - jeśli występuję więcej takich elementów, to usuwany jest pierwszy w kolejności
+listOfStrings.Remove("!");
 
-//tablice są indeksowane od 0 (minimalny indeks)
-//odwołujemy się do pierwszgo elemntu tablicy, czyli pod indeks 0
-array[0] = 5;
-array[1] = b;
-//maksymalny indeks tablicy to rozmiar minus 1 (3 - 1 = 2)
-//array[2] = c;
-array[array.Length - 1] = c;
+var indexOfExclamantionMark = listOfStrings.IndexOf("!"); //pytamy listę o index wskazanego elementu
+listOfStrings.Insert(indexOfExclamantionMark + 1, "@");
+
+//w listach odwołujemy się do elementów po indeksach (tak jak w tablicach)
+listOfStrings[listOfStrings.Count - 1] = "?";
 
 
-Console.WriteLine(array[2]);
-Console.WriteLine(array[1]);
-Console.WriteLine(array[0]);
+Console.WriteLine(listOfStrings[1]);
 
-Console.WriteLine($"Tablica ma rozmiar {array.Length}");
-
-string[] inputs = new string[2];
-Console.WriteLine("Podaj pierwszy bok prostokąta:");
-inputs[0] = Console.ReadLine();
-Console.WriteLine("Podaj drugi bok prostokąta:");
-inputs[1] = Console.ReadLine();
-
-float[] sides = new float[inputs.Length];
-sides[0] = float.Parse(inputs[0]);
-sides[1] = float.Parse(inputs[1]);
-
-float area = sides[0] * sides[1];
-Console.WriteLine($"Pole prostokąta to {area}");
-
-Console.WriteLine("Napisz coś:");
-string input = Console.ReadLine();
-//tablica zwracana jako rezultat wywołania metody
-//metoda split dzieli string na części wg wskazanego separatora
-
-//string[] words = input.Split(" ");
-string[] words = input.Split(); //bez parametru, działa jak powyżej
-
-Console.WriteLine($"Napisałeś {words.Length} słów. Pierwsze słowo: {words[0]}, ostatnie: {words[words.Length - 1]}");
+//przekształcenie tablicy do listy
+listOfStrings = Console.ReadLine().Split().ToList();
+Console.WriteLine($"Lista ma rozmiar: {listOfStrings.Count}");
