08096ab51dd725de6325afc4fac814dab28c81f8
Paweł Łęczycki <pawel@msd.info.pl>
2024-07-17 12:08:55 +0200

    Calculator

---
+++ ConsoleApp/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Calculator
+    {
+        private double _initialValue;
+
+        public Calculator() : this(0)
+        {
+        }
+
+        public Calculator(double initialValue)
+        {
+            Result = _initialValue = initialValue;
+        }
+
+        public double Result { get; private set; }
+        public int OperationCounter { get; private set; }
+
+        public void Add(double value)
+        {
+            Result += value;
+            IncreaseOperationCounter();
+        }
+
+        public void Substract(double value)
+        {
+            Result -= value;
+            IncreaseOperationCounter();
+        }
+        public void Multiply(double value)
+        {
+            Result = Result * value;
+            IncreaseOperationCounter();
+        }
+        public void Divide(double value)
+        {
+            Result = Result / value;
+            IncreaseOperationCounter();
+        }
+
+        private void IncreaseOperationCounter()
+        {
+            OperationCounter++;
+        }
+
+        public void Clear()
+        {
+            Result = _initialValue;
+            OperationCounter = 0;
+        }
+    }
+}
